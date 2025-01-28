2d9a57a281ad712479e65f3de8fd6c14d3fb73f8
vaibhav patil <vaibhavpatil2579@gmail.com>
2025-01-28 19:43:21 +0530

    changed the collection height

--- front-end/food-delivery/src/Components/collections/Template.jsx
+++ front-end/food-delivery/src/Components/collections/Template.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Template = ({img}) => {
   return (
-    <div className='w-[45%] sm:w-[23%] md:w-[23%] h-[200px] sm:h-[200px] md:h-[250px] bg-[#f1f1f1] rounded-xl overflow-hidden mb-4'>
+    <div className='w-[45%] sm:w-[23%] md:w-[23%] h-[200px] sm:h-[200px] md:h-[270px] bg-[#f1f1f1] rounded-xl overflow-hidden mb-4'>
         <img src={img} alt="" style={{objectFit:'fill', height:'100%', width:'100%'}}/>
     </div>
   )
