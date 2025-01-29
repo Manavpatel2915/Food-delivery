ce9b7edf46885ea1b79e46d52587b38ce3074f4f
vaibhav patil <vaibhavpatil2579@gmail.com>
2025-01-29 18:38:07 +0530

    fixed login

--- front-end/food-delivery/src/Components/Navbar/LoginButton.jsx
+++ front-end/food-delivery/src/Components/Navbar/LoginButton.jsx
@@ -1,10 +1,10 @@
 import React, {useContext} from "react";
 import { LoginContext } from "../../contexts/loginContext";
 export default function LoginButton(){
-    const {isOpen, setisOpen} = useContext(LoginContext);
+    const {isOpen, setisOpen , setRegister} = useContext(LoginContext);
     return(
         <>
-            <button className="cursor-pointer" onClick={()=>{setisOpen(true)}}>Log in</button>
+            <button className="cursor-pointer" onClick={()=>{setisOpen(true);setRegister(false)}}>Log in</button>
         </>
     );
 }
